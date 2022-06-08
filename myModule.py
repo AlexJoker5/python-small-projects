1e6eb9641de4137ea8836660ef5be3f6934ddbab
Christopher Lone Toe <79359116+AlexJoker5@users.noreply.github.com>
2022-06-08 16:57:08 +0630

    Add files via upload
    This is the Converter of converting these:
    Celsius to Fahrenheit
    Fahrenheit to Celsius
    Kilograms to pound
    Pound to kilograms
    Pound to Ounces
    Ounces to Pound
    Kilometers to miles
    Feet to inches
    Centimeters to meters

---
+++ myModule.py
@@ -0,0 +1,32 @@
+# Celsius to Fahrenheit
+import math
+
+
+def cToF(c=0):
+    return (c*9)/5 + 32
+# Fahrenheit to Celsius
+def fToC(f=0):
+    return ((f-32)*5)/9
+# Kilograms to pound
+def kgToLb(kg):
+    return kg*2.2
+# Pound to kilograms
+def lbToKg(lb):
+    return lb/2.2
+# Pound to Ounces
+def lbToOunce(lb):
+    return lb*16
+# Ounces to Pound
+def ounceToLb(ounce):
+    return ounce/16
+# Kilometers to miles
+def kmToMile(km):
+    return km/1.609
+# Feet to inches
+def ftToInch(ft=0):
+    return ft*12
+# Centimeters to meters
+def cmToM(cm):
+    return cm/100
+
+
